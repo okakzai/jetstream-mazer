32142eaf37bacf9f0bf776a2336c9f2794419db5
unknown <okakzai@gmail.com>
2025-03-14 22:24:54 +0700

    Favicon + Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Dashboard')</title>
+    <title>@yield('title', 'Dashboard') - {{ env('APP_NAME') }}</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('template/mazer/compiled/css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('template/mazer/compiled/css/app-dark.css') }}">
     {{-- Section untuk style tambahan --}}
